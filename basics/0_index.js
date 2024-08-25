d98ca24cb3689611957a64968e2e5664d3c00cdd
VISHAL SUBHASH BIRADAR <143383723+vishalsb1@users.noreply.github.com>
2024-08-25 17:28:21 +0000

    lerned about js datatypes and some keypointe

---
+++ basics/0_index.js
@@ -0,0 +1,17 @@
+console.log("vishal");
+
+// variable
+let a=90
+let b="nandini"
+let c='df'
+console.table([a,b,c]); //hyani apan max to max variable gheu shakto eksat ani table format madhi print karu shato
+
+console.log(typeof null); // nul in js is an object 
+
+// when we assin nothinf to an variable then we get that vales as undefned
+let val1;
+console.log(val1);
+console.log(typeof undefined);
+
+
+
---
+++ basics/1_index.js
@@ -0,0 +1,11 @@
+// value conversion 
+
+let score="33"
+
+let var2=Number(score)
+console.log(var2);
+
+let num=1
+let num1=Boolean(num);
+console.log(num1)
+console.log(typeof num1)
